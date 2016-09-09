8d6eacb8a8608c6f2053c44b60a91392aafca233
erenMustafaOzdal <eren.060737@gmail.com>
2016-09-09 14:16:29 +0000

    product category işlemleri bitti

--- src/Http/Controllers/ProductCategoryApiController.php
+++ src/Http/Controllers/ProductCategoryApiController.php
@@ -40,20 +40,6 @@ class ProductCategoryApiController extends BaseNodeController
     }
 
     /**
-     * get detail
-     *
-     * @param integer $id
-     * @param Request $request
-     * @return \Illuminate\Http\Response
-     */
-    public function detail($id, Request $request)
-    {
-        return ProductCategory::where('id', $id)
-            ->select('type')
-            ->first();
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  ApiStoreRequest  $request
--- src/Http/Controllers/ProductCategoryController.php
+++ src/Http/Controllers/ProductCategoryController.php
@@ -26,161 +26,90 @@ class ProductCategoryController extends BaseNodeController
     /**
      * Display a listing of the resource.
      *
-     * @param integer|null $id
      * @return \Illuminate\Http\Response
      */
-    public function index($id = null)
+    public function index()
     {
-        if (is_null($id)) {
-            return view(config('laravel-product-module.views.product_category.index'));
-        }
-
-        $parent_product_category = ProductCategory::findOrFail($id);
-        return view(config('laravel-product-module.views.product_category.index'), compact('parent_product_category'));
+        return view(config('laravel-product-module.views.product_category.index'));
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @param \Illuminate\Http\Request $request
-     * @param integer|null $id
      * @return \Illuminate\Http\Response
      */
-    public function create(Request $request, $id = null)
+    public function create()
     {
-        // eğer product ids var ve token hatalı ise hata döndür
-        if ( $request->has('product_ids') && ( ! $request->has('_token') || session('_token') !== $request->input('_token') ) ) {
-            abort(403);
-        }
-
-        // product ids var ise oluştur
-        $products = $request->has('product_ids') ? Product::with('video','photo')->whereIn('id', explode(',', $request->product_ids))->get() : collect();
-
         $operation = 'create';
-        if (is_null($id)) {
-            return view(config('laravel-product-module.views.product_category.create'), compact('operation','products'));
-        }
-
-        $parent_product_category = ProductCategory::findOrFail($id);
-        $type = $parent_product_category->type;
-        $types = $products->groupBy('type');
-
-        // kategori olduğu için; gelen medyalar kategorinin tipine uygun olmalı
-        if ( $type !== 'mixed' && ($types->count() === 1 && $types->keys()->first() !== $type) ) {
-            Flash::error(lmcTrans('laravel-product-module/admin.flash.product_incompatible', [
-                'type' => lmcTrans("laravel-product-module/admin.fields.product.{$type}")
-            ]));
-            return back();
-        }
-        return view(config('laravel-product-module.views.product_category.create'), compact('parent_product_category','operation','products'));
+        return view(config('laravel-product-module.views.product_category.create'), compact('operation','products'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  StoreRequest  $request
-     * @param integer|null $id
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreRequest $request, $id = null)
+    public function store(StoreRequest $request)
     {
         $this->setEvents([
             'success'   => StoreSuccess::class,
             'fail'      => StoreFail::class
         ]);
-        if (is_null($id)) {
-            $redirect = 'index';
-            return $this->storeModel(ProductCategory::class,$redirect);
-        }
-        $redirect = 'product_category.product_category.index';
-        $this->setRelationRouteParam($id, config('laravel-product-module.url.product_category'));
-        $this->setDefineValues(['type']);
-        return $this->storeNode(ProductCategory::class,$redirect);
+        return $this->storeModel(ProductCategory::class,'index');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param integer|ProductCategory $firstId
-     * @param integer|null $secondId
+     * @param ProductCategory $product_category
      * @return \Illuminate\Http\Response
      */
-    public function show($firstId, $secondId = null)
+    public function show($product_category)
     {
-        $product_category = is_null($secondId) ? $firstId : $secondId;
-        if (is_null($secondId)) {
-            return view(config('laravel-product-module.views.product_category.show'), compact('product_category'));
-        }
-
-        $parent_product_category = ProductCategory::findOrFail($firstId);
-        return view(config('laravel-product-module.views.product_category.show'), compact('parent_product_category','product_category'));
+        return view(config('laravel-product-module.views.product_category.show'), compact('product_category'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param integer|ProductCategory $firstId
-     * @param integer|null $secondId
+     * @param ProductCategory $product_category
      * @return \Illuminate\Http\Response
      */
-    public function edit($firstId, $secondId = null)
+    public function edit($product_category)
     {
         $operation = 'edit';
-        $product_category = is_null($secondId) ? $firstId : $secondId;
-        if (is_null($secondId)) {
-            return view(config('laravel-product-module.views.product_category.edit'), compact('product_category','operation'));
-        }
-
-        $parent_product_category = ProductCategory::findOrFail($firstId);
-        return view(config('laravel-product-module.views.product_category.edit'), compact('parent_product_category','product_category','operation'));
+        return view(config('laravel-product-module.views.product_category.edit'), compact('product_category','operation'));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  UpdateRequest  $request
-     * @param integer|ProductCategory $firstId
-     * @param integer|null $secondId
+     * @param ProductCategory $product_category
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdateRequest $request, $firstId, $secondId = null)
+    public function update(UpdateRequest $request, $product_category)
     {
-        $product_category = is_null($secondId) ? $firstId : $secondId;
-        if (is_null($secondId)) {
-            $redirect = 'show';
-        } else {
-            $redirect = 'product_category.product_category.show';
-            $this->setRelationRouteParam($firstId, config('laravel-product-module.url.product_category'));
-        }
-
         $this->setEvents([
             'success'   => UpdateSuccess::class,
             'fail'      => UpdateFail::class
         ]);
-        return $this->updateModel($product_category, $redirect);
+        return $this->updateModel($product_category, 'show');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param integer|ProductCategory $firstId
-     * @param integer|null $secondId
+     * @param ProductCategory $product_category
      * @return \Illuminate\Http\Response
      */
-    public function destroy($firstId, $secondId = null)
+    public function destroy($product_category)
     {
-        $product_category = is_null($secondId) ? $firstId : $secondId;
-        if (is_null($secondId)) {
-            $redirect = 'index';
-        } else {
-            $redirect = 'product_category.product_category.index';
-            $this->setRelationRouteParam($firstId, config('laravel-product-module.url.product_category'));
-        }
-
         $this->setEvents([
             'success'   => DestroySuccess::class,
             'fail'      => DestroyFail::class
         ]);
-        return $this->destroyModel($product_category, $redirect);
+        return $this->destroyModel($product_category, 'index');
     }
 }
