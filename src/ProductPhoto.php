3caba22308b7125ad2a770ba80fe4f6f7b1acfc1
erenMustafaOzdal <eren.060737@gmail.com>
2016-09-18 05:40:16 +0000

    model photo remove işlemi tamamlandı

--- src/ProductPhoto.php
+++ src/ProductPhoto.php
@@ -4,6 +4,7 @@ namespace ErenMustafaOzdal\LaravelProductModule;
 
 use Illuminate\Database\Eloquent\Model;
 use ErenMustafaOzdal\LaravelModulesBase\Traits\ModelDataTrait;
+use ErenMustafaOzdal\LaravelModulesBase\Repositories\FileRepository;
 
 class ProductPhoto extends Model
 {
@@ -70,4 +71,33 @@ class ProductPhoto extends Model
     | Model Set and Get Attributes
     |--------------------------------------------------------------------------
     */
+
+
+
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Model Events
+    |--------------------------------------------------------------------------
+    */
+
+    /**
+     * model boot method
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        /**
+         * model deleted method
+         *
+         * @param $model
+         */
+        parent::deleted(function($model)
+        {
+            $file = new FileRepository(config('laravel-product-module.product.uploads'));
+            $file->deletePhoto($model, 'product');
+        });
+    }
 }
